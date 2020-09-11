f9fb955a7ee33704b158c4df21b776579b9ced31
hsgilliland <jo-ash@procentrica.co.za>
2020-09-11 14:41:31 +0200

    handler complete

--- src/handlers.go
+++ src/handlers.go
@@ -1,1 +1,91 @@
 package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func (s *Server) handleexportasset() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("Handle Export Asset Has Been Called...")
+		// retrieving the ID of the asset that is requested.
+		exportAsset := ExportAsset{}
+		// convert received JSON payload into the declared struct.
+		err := json.NewDecoder(r.Body).Decode(&exportAsset)
+		//check for errors when converting JSON payload into struct.
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprintf(w, "Bad JSON provided to export asset")
+			return
+		}
+
+		//create byte array from JSON payload
+		requestByte, _ := json.Marshal(exportAsset)
+
+		//post to crud service
+		req, respErr := http.Post("http://"+config.CRUDHost+":"+config.CRUDPort+"/assetregister", "application/json", bytes.NewBuffer(requestByte))
+
+		//check for response error of 500
+		if respErr != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, respErr.Error())
+			fmt.Println("Error in communication with CRUD service endpoint for export asset")
+			return
+		}
+		if req.StatusCode != 200 {
+			fmt.Fprint(w, "Request to DB can't be completed...")
+			fmt.Println("Unable to asset export")
+		}
+		if req.StatusCode == 500 {
+			w.WriteHeader(500)
+
+			bodyBytes, err := ioutil.ReadAll(req.Body)
+			if err != nil {
+				log.Fatal(err)
+			}
+			bodyString := string(bodyBytes)
+			fmt.Fprintf(w, "Request to DB can't be completed..."+bodyString)
+			fmt.Println("Request to DB can't be completed..." + bodyString)
+			return
+		}
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, err.Error())
+			fmt.Println("Exportation is not able to be completed by internal error")
+			return
+		}
+
+		//close the request
+		defer req.Body.Close()
+
+		//create new response struct
+		var assetResponse ExportAssetResponse
+
+		//decode request into decoder which converts to the struct
+		decoder := json.NewDecoder(req.Body)
+
+		err = decoder.Decode(&assetResponse)
+		if err != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, err.Error())
+			fmt.Println("Error occured in decoding asset response")
+			return
+		}
+		js, jserr := json.Marshal(assetResponse)
+		if jserr != nil {
+			w.WriteHeader(500)
+			fmt.Fprint(w, jserr.Error())
+			fmt.Println("Error occured when trying to marshal the response to export asset")
+			return
+		}
+
+		//return back to Front-End user
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(200)
+		w.Write(js)
+	}
+}
--- src/types.go
+++ src/types.go
@@ -2,7 +2,7 @@ package main
 
 import "github.com/gorilla/mux"
 
-//touter service struct
+//router service struct
 type Server struct {
 	router *mux.Router
 }
@@ -12,4 +12,28 @@ type Config struct {
 	CRUDHost            string
 	CRUDPort            string
 	GENESISEXPORTERPort string
+}
+
+type ExportAsset struct {
+	AssetTypeID string `json:"id"`
+}
+
+type ExportAssetResponse struct {
+	AssettypeLevelID      string `json:"level"`
+	Code                  string `json:"code"`
+	Name                  string `json:"name"`
+	Description           string `json:"description"`
+	IsUTC                 bool   `json:"isutc"`
+	SizeUnit              string `json:"sizeunit"`
+	TypeLookup            string `json:"typelookup"`
+	SizeLookup            string `json:"sizelookup"`
+	Dimension1Name        string `json:"dimension1name"`
+	Dimension1Description string `json:"dimension1description"`
+	Dimension1Unit        string `json:"dimension1unit"`
+	Dimension2Name        string `json:"dimension1name"`
+	Dimension2Description string `json:"dimension2description"`
+	ExtentFormula         string `json:"extentformula"`
+	DepreciationModel     string `json:"depreciationmodel"`
+	DepreciationMethod    string `json:"depreciationmethod"`
+	ISActive              bool   `json:"isactive"`
 }
